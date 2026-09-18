2becb2a0192feedb78ea2cddbb7d046a0fa5c449
kevin7548 <jongmin.kim.k@gmail.com>
2026-09-18 18:01:35 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.67 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -7,12 +7,12 @@ int solution(int num) {
     int answer = 0;
     long long N = (long long)num;
     
-    while (N > 1 and answer < 500) {
+    while (N > 1 && answer < 500) {
         if (N % 2 == 0)
             N = N / 2;
         else
             N = N * 3 + 1;
-        answer += 1;
+        answer++;
     }
     
     if (N == 1)
