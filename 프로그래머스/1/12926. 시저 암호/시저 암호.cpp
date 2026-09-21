8732af5977bbfd83294bcd214ac00b4326903a7e
kevin7548 <jongmin.kim.k@gmail.com>
2026-09-21 17:01:32 +0900

    [level 1] Title: 시저 암호, Time: 0.03 ms, Memory: 4.77 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -1,27 +1,22 @@
 #include <string>
 #include <vector>
-#include <cctype>
 
 using namespace std;
 
 string solution(string s, int n) {
     for (char &ch : s) {
         // 공백이면 continue
-        if (isspace(ch))
+        if (ch == ' ')
             continue;
         
-        int idx = ch + n;
         // 알파벳 소문자 (97~122)
-        if ('a' <= ch && ch <= 'z') {
-            if (idx > 122)
-                idx -= 26;
+        else if ('a' <= ch && ch <= 'z') {
+            ch = 'a' + (ch - 'a' + n) % 26;
         }
         // 알파벳 대문자 (65~90)
         else if ('A' <= ch && ch <= 'Z') {
-            if (idx > 90)
-                idx -= 26;
+            ch = 'A' + (ch - 'A' + n) % 26;
         }
-        ch = (char)idx;
     }
     return s;
 }
